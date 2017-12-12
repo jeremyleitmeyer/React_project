6dfa39b820e53e7b795bdc21184529d84c2a9cb8
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-12-12 10:38:12 -0500

    more auth

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ const keys = require('./config/keys')
 
 const app = express();
 
+// import google cred and set callback route after getting google profile
 passport.use(
 	new GoogleStrategy({
 		clientID: keys.googleClientID,
 		clientSecret: keys.googleClientSecret,
 		callbackURL: '/auth/google/callback'
-	}, (accessToken) => {
+	}, (accessToken, refreshToken, profile, done) => {
 		console.log(accessToken);
+		console.log(refreshToken);
+		console.log(profile);
 	})
 );
 
+
+// 'google' is set internally by GoogleStrategy
+app.get('/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// set the port for deployment or local
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+
+app.listen(PORT, function(){
+		console.log('Running on: ' + PORT)
+});
